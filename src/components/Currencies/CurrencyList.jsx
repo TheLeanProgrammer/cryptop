427d5f1119ddfb53972dbd7e819da8dc52f8a91c
MadhavBahl <madhavbahl10@gmail.com>
2022-02-10 20:42:26 +0530

    Add currency card

--- src/components/Currencies/CurrencyList.jsx
+++ src/components/Currencies/CurrencyList.jsx
@@ -7,6 +7,24 @@ const CurrencyList = () => {
             <h1 className='currency-section-heading'>
                 Today's top 10 currencies
             </h1>
+
+            <div className='crypto-cards-container'>
+                <div className='crypto-card'>
+                    <img
+                        className='currency-logo'
+                        src='https://cryptoicon-api.vercel.app/api/icon/usdt'
+                        alt='crypto-icon'
+                    />
+                    <div className='coin-name'>
+                        <span className='coin-name-title'>Bitcoin </span>
+                        <span className='coin-symbol'>&nbsp;BTC</span>
+                    </div>
+                    <div className='coin-price'>
+                        <span className='coin-dollar'>$</span>
+                        <span className='coin-price-value'>37543.2</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
